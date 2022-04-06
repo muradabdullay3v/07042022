07309a399996f074ea1942bdf85989695e5df549
muradabdullay3v <murad.abdullazade.53@mail.ru>
2022-04-07 02:42:34 +0400

    created

---
+++ Home06042022/CustomCollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home06042022
+{
+    internal class CustomCollection<TKey,TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+        CustomCollection<TKey, TValue>[] array;
+        public void Add(TKey key , TValue value)
+        {
+            Array.Resize(ref array , array.Length + 1);
+            array[^1].Value = value;    
+            array[^1].Key = key;  
+        }
+    }
+}
---
+++ Home06042022/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace Home06042022
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            SortedList<int, string> List = new SortedList<int, string>();
+            List.Add(2001, "C#");
+            List.Add(2014, "Swift");
+            List.Add(1990, "Python");
+            List.Add(1995, "JavaScript");
+            List.Add(1980, "C++");
+
+            foreach (var item in List)
+            {
+                Console.WriteLine($"Yaranma tarixi : {item.Key}\nProgramlashdirma dili : {item.Value}\n");
+            }
+
+        }
+    }
+}
